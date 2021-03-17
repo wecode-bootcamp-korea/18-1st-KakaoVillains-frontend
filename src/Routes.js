dfce45e3043455d16838a79c9c506ec72d0305e2
JunWoo <kjwoo9595@gmail.com>
2021-03-17 20:07:07 +0900

    불필요 파일 삭제

--- src/Routes.js
+++ src/Routes.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Main from './Pages/Main/Main';
 import Login from './Pages/Login/Login';
 import SignUp from './Pages/Login/SignUp/SignUp';
-import Products from './Pages/Products/Products';
 
 class Routes extends React.Component {
   render() {
@@ -13,7 +12,6 @@ class Routes extends React.Component {
           <Route exact path="/" component={Main} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={SignUp} />
-          <Route exact path="/products" component={Products} />
         </Switch>
       </Router>
     );
