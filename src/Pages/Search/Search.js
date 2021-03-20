22815efbd6d8f05ad3d1e9d48bcc090409801cf8
songbetter <5ongnaeu17@gmail.com>
2021-03-21 00:37:43 +0900

    Add: SearchBox 취소, 삭제 버튼 기능 추가

--- src/Pages/Search/Search.js
+++ src/Pages/Search/Search.js
@@ -2,15 +2,66 @@ import React from 'react';
 import CharacterBox from './CharacterBox/CharacterBox';
 import CategoryBox from './CategoryBox/CategoryBox';
 import SearchBox from './SearchBox/SearchBox';
+import SearchDataList from './SearchDataList/SearchDataList';
 import './search.scss';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      keyWord: '',
+      keyWordList: [],
+      toggleOff: false,
+    };
+  }
+
+  keyWordInput = e => {
+    this.setState({
+      keyWord: e.target.value,
+    });
+  };
+
+  keyWordDelete = () => {
+    this.setState({
+      keyWord: '',
+    });
+  };
+
+  filterKeyword = () => {
+    fetch(`/data/searchData.json?filter${this.state.keyword}`)
+      .then(res => res.json())
+      .then(res =>
+        this.setState({
+          keyWordList: res,
+        })
+      );
+  };
+
+  goToBack = () => {
+    this.setState({
+      toggleOff: true,
+    });
+  };
+
   render() {
+    const { keyWord, keyWordList, toggleOff } = this.state;
+    const { keyWordDelete, keyWordInput, goToBack } = this;
     return (
-      <main className="search">
-        <SearchBox />
-        <CharacterBox />
-        <CategoryBox />
+      <main className={toggleOff ? 'searchOff' : 'search'}>
+        <SearchBox
+          keyWordDelete={keyWordDelete}
+          keyWordInput={keyWordInput}
+          keyWord={keyWord}
+          goToBack={goToBack}
+        />
+        {keyWord !== '' ? (
+          <SearchDataList keyWordList={keyWordList} />
+        ) : (
+          <div>
+            <CharacterBox />
+            <CategoryBox />
+          </div>
+        )}
       </main>
     );
   }
--- src/Pages/Search/SearchBox/SearchBox.js
+++ src/Pages/Search/SearchBox/SearchBox.js
@@ -4,13 +4,17 @@ import './searchBox.scss';
 
 class SearchBox extends React.Component {
   render() {
+    const { keyWord, keyWordInput, keyWordDelete, goToBack } = this.props;
     return (
       <section className="searchBox">
         <div className="iconBox">
           <BiSearch className="searchIcon" />
-          <input type="text" />
+          <input type="text" onChange={keyWordInput} value={keyWord} />
+          <button className={keyWord !== '' && 'btnOn'} onClick={keyWordDelete}>
+            X
+          </button>
         </div>
-        <button>취소</button>
+        <button onClick={goToBack}>취소</button>
       </section>
     );
   }
