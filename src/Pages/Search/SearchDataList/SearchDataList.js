3077062ff5a15d9448f50c39d9600554209e2d27
songbetter <5ongnaeu17@gmail.com>
2021-03-21 00:36:13 +0900

    Add: SearchDataList 페이지 추가

---
+++ src/Pages/Search/SearchDataList/SearchDataList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './searchDataList.scss';
+
+class SearchDataList extends React.Component {
+  render() {
+    const { keyWordList } = this.props;
+    return (
+      <section className="searchDataList">
+        {!false ? (
+          <div className="noResult">검색결과가 없습니다.</div>
+        ) : (
+          <ul>
+            {keyWordList.map(data => (
+              <li key={data.id}>
+                <Link to={`/product/${data.id}`}>{data.name}</Link>
+              </li>
+            ))}
+          </ul>
+        )}
+      </section>
+    );
+  }
+}
+
+export default SearchDataList;
