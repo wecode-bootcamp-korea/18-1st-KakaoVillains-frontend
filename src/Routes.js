3c28459197244acf48d08c7be39da3187a34e2f6
songbetter <5ongnaeu17@gmail.com>
2021-03-21 00:27:37 +0900

    Add: 라우터 기능 추가

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Main from './Pages/Main/Main';
+import Search from './Pages/Search/Search';
+import Test from './Pages/Search/Test';
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={Main} />
+          <Route exact path="/" component={Search} />
+          <Route exact path="/category/subject/:id" component={Test} />
+          <Route exact path="/category/character/:id" component={Test} />
+          <Route exact path="/product/:id" component={Test} />
         </Switch>
       </Router>
     );
