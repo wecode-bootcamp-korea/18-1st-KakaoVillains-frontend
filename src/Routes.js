1a26f293ed24f3976eee4c1e5c4290857b16d505
songbetter <5ongnaeu17@gmail.com>
2021-03-21 20:53:50 +0900

    Add: 라우터 기능 추가

--- src/Routes.js
+++ src/Routes.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Search from './Pages/Search/Search';
-import Test from './Pages/Search/Test';
+import Character from './Pages/Category/Character/Character';
+import Subject from './Pages/Category/Subject/Subject';
+import Category from './Pages/Category/Category';
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={Search} />
-          <Route exact path="/category/subject/:id" component={Test} />
-          <Route exact path="/category/character/:id" component={Test} />
-          <Route exact path="/product/:id" component={Test} />
+          <Route exact path="/category/character" component={Character} />
+          <Route
+            exact
+            path="/category/character/:character"
+            component={Character}
+          />
+          <Route
+            exact
+            path="/category/character/:character/:sort"
+            component={Character}
+          />
+          <Route exact path="/category/subject" component={Subject} />
+          <Route exact path="/category/subject/:subject" component={Subject} />
+          <Route
+            exact
+            path="/category/subject/:subject/:subcategory"
+            component={Subject}
+          />
+          <Route
+            exact
+            path="/category/subject/:subject/:subcategory/:character"
+            component={Subject}
+          />
+          <Route
+            exact
+            path="/category/subject/:subject/:subcategory/:character/:sort"
+            component={Subject}
+          />
+          <Route exact path="/" component={Category} />
         </Switch>
       </Router>
     );
