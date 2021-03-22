1e24a463af4aebfd63a1e2504dafc7620650210a
Jieun-yyang <poohv7@gmail.com>
2021-03-22 12:02:08 +0900

    피드백 수정, ul, li 태그를 사용해서 semantic 하게 변경

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,15 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Main from "./Pages/Main/Main";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+//import Main from './Pages/Main/Main';
+import Footer from './Components/Footer';
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={Main} />
+          {/* <Route exact path="/" component={Main} /> */}
+          <Route exact path="/footer" component={Footer} />
         </Switch>
       </Router>
     );
