1a0aec54d78fe7342c534721e6141fadc5a4dd35
songbetter <5ongnaeu17@gmail.com>
2021-03-23 11:17:04 +0900

    Resolve Conflicts

--- src/Routes.js
+++ src/Routes.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Search from './Pages/Search/Search';
-import Test from './Pages/Search/Test';
 import Main from './Pages/Main/Main';
 import Login from './Pages/Login/Login';
 import SignUp from './Pages/Login/SignUp/SignUp';
+import Search from './Pages/Search/Search';
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={Search} />
-          <Route exact path="/category/subject/:id" component={Test} />
-          <Route exact path="/category/character/:id" component={Test} />
-          <Route exact path="/product/:id" component={Test} />
           <Route exact path="/" component={Main} />
+          <Route exact path="/Search" component={Search} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={SignUp} />
         </Switch>
