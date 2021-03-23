2c70129a82bae5c8d30d20c4d53d93c251c67440
Jieun-yyang <poohv7@gmail.com>
2021-03-23 10:59:27 +0900

    Route.js, index.js 파일 수정

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-//import Main from './Pages/Main/Main';
+import Main from './Pages/Main/Main';
+import Login from './Pages/Login/Login';
+import SignUp from './Pages/Login/SignUp/SignUp';
 import Footer from './Components/Footer';
 
 class Routes extends React.Component {
@@ -8,7 +10,9 @@ class Routes extends React.Component {
     return (
       <Router>
         <Switch>
-          {/* <Route exact path="/" component={Main} /> */}
+          <Route exact path="/" component={Main} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
           <Route exact path="/footer" component={Footer} />
         </Switch>
       </Router>
