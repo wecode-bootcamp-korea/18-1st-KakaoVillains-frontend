33c16f3dd45c976b9027c1936462518990cb5bed
JunWoo <kjwoo9595@gmail.com>
2021-03-24 13:51:17 +0900

    추천상품 grid로 정렬

--- src/Pages/Products/Components/Relative/Relative.js
+++ src/Pages/Products/Components/Relative/Relative.js
@@ -4,22 +4,28 @@ import { FiShoppingBag } from 'react-icons/fi';
 class Relative extends React.Component {
   render() {
     return (
-      <div className="productRecommend">
-        <p className="recommendTitle">
-          잠깐만,
-          <br />이 상품은 어때요?
-        </p>
-        {this.props.related_products.map((product, idx) => (
-          <div className="recommendProduct" key={idx}>
-            <img src={product.image_url} alt="productImg1" />
-            <div className="nameBuyBox">
-              <p className="recommendName">{product.name}</p>
-              <FiShoppingBag className="shoppingIcon" />
+      <>
+        <div className="productRecommend">
+          <p className="recommendTitle">
+            잠깐만,
+            <br />이 상품은 어때요?
+          </p>
+        </div>
+        <div className="gridBox">
+          {this.props.related_products.map((product, idx) => (
+            <div className="recommendProduct" key={idx}>
+              <img src={product.image_url} alt="productImg1" />
+              <div className="nameBuyBox">
+                <p className="recommendName">{product.name}</p>
+                <FiShoppingBag className="shoppingIcon" />
+              </div>
+              <p className="recommendPrice">
+                {product.price.toLocaleString()}원
+              </p>
             </div>
-            <p className="recommendPrice">{product.price.toLocaleString()}원</p>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      </>
     );
   }
 }
