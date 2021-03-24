8184738886eb955291b2a4ab27aa4662a4b59c3a
JunWoo <kjwoo9595@gmail.com>
2021-03-24 14:54:19 +0900

    함수형 컴포넌트 클래스형으로 수정

--- src/Pages/Products/Components/Rating/Rating.js
+++ src/Pages/Products/Components/Rating/Rating.js
@@ -1,20 +1,24 @@
 import React from 'react';
 
-function getStars(value) {
-  const stars = [];
-  const [whole, part] = parseFloat(value).toString().split('.');
-  for (let i = 0; i < whole; i++) stars.push(100);
-  if (part) stars.push(50);
-  for (let i = whole; i < (part ? 4 : 5); i++) stars.push(0);
-  return stars;
+class Rating extends React.Component {
+  getStars = value => {
+    const stars = [];
+    const [whole, part] = parseFloat(value).toString().split('.');
+    for (let i = 0; i < whole; i++) stars.push(100);
+    if (part) stars.push(50);
+    for (let i = whole; i < (part ? 4 : 5); i++) stars.push(0);
+    return stars;
+  };
+  render() {
+    console.log(this.getStars());
+    return (
+      <div className="star">
+        {this.getStars(this.props.value).map(value => (
+          <img src={`/images/${value}.png`} alt="stars" />
+        ))}
+      </div>
+    );
+  }
 }
 
-export default function Rating({ value }) {
-  return (
-    <div className="star">
-      {getStars(value).map((value, idx) => (
-        <img src={`/images/${value}.png`} key={idx} alt="stars" />
-      ))}
-    </div>
-  );
-}
+export default Rating;
--- src/Pages/Products/Products.js
+++ src/Pages/Products/Products.js
@@ -33,15 +33,6 @@ class Products extends React.Component {
     });
   };
 
-  getStars = value => {
-    const stars = [];
-    const [whole, part] = parseFloat(value).toString().split('.');
-    for (let i = 0; i < whole; i++) stars.push(100);
-    if (part) stars.push(50);
-    for (let i = whole; i < (part ? 4 : 5); i++) stars.push(0);
-    return stars;
-  };
-
   componentDidMount() {
     fetch('/data/ProductData.json', {
       methhod: 'GET',
